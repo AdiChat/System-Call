e72365127262c22e37971f8453e3cb4a3f6e62a0
Aditya Chatterjee <aditianhacker@gmail.com>
2016-09-10 20:18:18 +0530

    Created ruby gem

---
+++ Gem System_Call/lib/system_call.rb
@@ -0,0 +1,48 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+require 'action_view'
+
+class System_Call
+
+	def  generate_list()
+
+		$stdout = File.new('E:/System Calls in UNIX.txt', 'a')
+		$stdout.sync = true
+		addr = "http://linasm.sourceforge.net/docs/syscalls"
+		new_page = Nokogiri::HTML(open("http://linasm.sourceforge.net/docs/syscalls/index.php"))
+		moji = new_page.css('aside')
+		moji1 = moji.css('a').map { |link| link['href'] }
+		$count_1 = moji1.length
+		$counting = 1
+		while $counting < $count_1
+			$full = "http://linasm.sourceforge.net/docs/syscalls/" + moji1[$counting]
+			$counting = $counting + 1
+			pageo = Nokogiri::HTML(open($full))
+			puts pageo.css('title').text
+			sho = pageo.css('tr')
+			$count = 1
+			$countx = sho.length
+			while $count < $countx
+				link1 = sho[$count].css('a').map { |link| link['href'] }
+				if !link1[0]
+					$count = $count + 1
+					next
+				end		
+				puts link1[0]	
+				page = Nokogiri::HTML(open(link1[0])) 
+				sh = page.css('pre')
+				puts sh.length
+				puts "SYSTEM CALL:"
+				puts page.css('title').text
+				puts "FUNCTIONALITY:"
+				puts sh[1].text
+				puts "SYNOPSIS:"
+				puts sh[2].text
+				puts "DESCRIPTION"
+				puts sh[3].text
+				$count = $count + 1
+			end
+		end
+	end
+end
---
+++ Gem System_Call/system_call.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = 'system_call'
+  s.version     = '1.0.0'
+  s.date        = '2016-08-10'
+  s.summary     = "The system_call gem provides a way to scrape data from linasm.sourceforge.net to create a list of system calls used in UNIX based systems"
+  s.description = "A gem for creating a list of system calls used in UNIX based systems"
+  s.authors     = ["Aditya Chatterjee"]
+  s.email       = 'aditianhacker@gmail.com'
+  s.files       = ["lib/system_call.rb"]
+  s.homepage    =
+    'http://rubygems.org/gems/system_call'
+  s.license       = 'CC0-1.0'
+end
